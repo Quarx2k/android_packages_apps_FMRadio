9e9b48b3797b1450b53bbe0e29acc51aeef4bda9
Nickolay Semendyaev <agent00791@gmail.com>
2016-09-14 22:56:44 +0600

    Add test fm binary

--- fmradio.v4l2-fm/Android.mk
+++ fmradio.v4l2-fm/Android.mk
@@ -9,3 +9,14 @@ LOCAL_MODULE_TAG := optional
 LOCAL_SHARED_LIBRARIES := liblog
 LOCAL_SRC_FILES := v4l2_fm.c v4l2_ioctl.c v4l2_ioctl.h
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := radio_v4l
+LOCAL_SRC_FILES := radio_v4l.c
+LOCAL_MODULE_TAG := optional
+LOCAL_SHARED_LIBRARIES := libtinyalsa libfmradio.v4l2-fm
+LOCAL_C_INCLUDES := \
+	external/tinyalsa/include \
+	hardware/libhardware/include
+include $(BUILD_EXECUTABLE)
